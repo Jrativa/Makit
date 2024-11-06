ff715551b21d2f7a3580ef716297786f34fbe203
Jose <jrativa@unal.edu.co>
2024-11-06 14:59:07 +0100

    Deployment settings

--- src/components/CompaniesList.jsx
+++ src/components/CompaniesList.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
 const companies = [
-  { id: 1, name: "BuildSmart Consulting", description: "Offering expert consulting in construction project management and sustainable building practices.", services: ["Project Management", "Cost Estimation", "Quality Assurance"], category: "Architects", logo: "https://via.placeholder.com/100x100?text=BuildSmart", href:"/profile" },
-  { id: 2, name: "HomeRevamp Interiors", description: "Specializing in interior remodeling, from kitchens to living spaces with a modern design touch.", services: ["Interior Design", "Kitchen Remodeling", "Bathroom Renovation"], category: "Interior Designers", logo: "https://via.placeholder.com/100x100?text=HomeRevamp", href:"/profile" },
-  { id: 3, name: "EcoBuild Solutions", description: "Focused on eco-friendly construction and sustainable building materials for a greener future.", services: ["Green Building Consulting", "Energy Efficient Design", "Sustainable Materials"], category: "Architects", logo: "https://via.placeholder.com/100x100?text=EcoBuild",  href:"/profile" },
-  { id: 4, name: "PrimeElectric", description: "Licensed electricians for residential and commercial projects, specializing in wiring and lighting.", services: ["Residential Wiring", "Commercial Lighting", "Electrical Repairs"], category: "Electricians", logo: "https://via.placeholder.com/100x100?text=PrimeElectric", href:"/profile" },
-  { id: 5, name: "FlowFix Plumbing", description: "Experienced plumbers providing solutions for homes and businesses, from leaks to full installations.", services: ["Pipe Installation", "Leak Repair", "Bathroom Plumbing"], category: "Plumbers", logo: "https://via.placeholder.com/100x100?text=FlowFix", href:"/profile" },
+  { id: 1, name: "BuildSmart Consulting", description: "Offering expert consulting in construction project management and sustainable building practices.", services: ["Project Management", "Cost Estimation", "Quality Assurance"], category: "Architects", logo: "/logo.png", href:"/profile", premium: true },
+  { id: 2, name: "HomeRevamp Interiors", description: "Specializing in interior remodeling, from kitchens to living spaces with a modern design touch.", services: ["Interior Design", "Kitchen Remodeling", "Bathroom Renovation"], category: "Interior Designers", logo: "/logo1.png", href:"/profile" },
+  { id: 3, name: "EcoBuild Solutions", description: "Focused on eco-friendly construction and sustainable building materials for a greener future.", services: ["Green Building Consulting", "Energy Efficient Design", "Sustainable Materials"], category: "Architects", logo: "/logo2.png",  href:"/profile" },
+  { id: 4, name: "PrimeElectric", description: "Licensed electricians for residential and commercial projects, specializing in wiring and lighting.", services: ["Residential Wiring", "Commercial Lighting", "Electrical Repairs"], category: "Electricians", logo: "/logo3.png", href:"/profile" },
+  { id: 5, name: "FlowFix Plumbing", description: "Experienced plumbers providing solutions for homes and businesses, from leaks to full installations.", services: ["Pipe Installation", "Leak Repair", "Bathroom Plumbing"], category: "Plumbers", logo: "/logo.png", href:"/profile" },
 ];
 
 const categories = ["Architects", "Interior Designers", "Electricians", "Plumbers"];
@@ -78,6 +78,28 @@ function CompanyList() {
                       <li key={index}>{service}</li>
                     ))}
                   </ul>
+                </div>
+                <div style={{marginLeft:'10%', marginBottom:'6%'}}>
+                  {company.premium && (
+                    <div className="bg-redMakit text-white p-2 rounded-full text-sm" >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="size-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M9 12.75L11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z"
+                        />
+                      </svg>
+
+                    </div>
+                      
+                  )}
                 </div>
               </a>
             ))}
